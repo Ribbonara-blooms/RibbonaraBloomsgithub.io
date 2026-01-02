d16270153ec768949f5e6655fb4557447329dad5
Ribbonara-blooms <sthbeeshal@gmail.com>
2026-01-02 20:59:03 +0545

    Create product.js

---
+++ js/js/js/product.js
@@ -0,0 +1,31 @@
+// EDIT PRICE, IMAGE & NAME HERE 👇
+const products = [
+  {
+    id: 1,
+    name: "Red Rose Love",
+    price: 2500, // PRICE IN NPR
+    image: "images/rose.jpg", // IMAGE PATH
+    description: "Classic red roses for love & romance"
+  },
+  {
+    id: 2,
+    name: "Pink Lily Grace",
+    price: 3000,
+    image: "images/lily.jpg",
+    description: "Soft pink lilies with elegant beauty"
+  },
+  {
+    id: 3,
+    name: "Tulip Dream",
+    price: 2800,
+    image: "images/tulip.jpg",
+    description: "Pastel tulips for sweet moments"
+  },
+  {
+    id: 4,
+    name: "Mixed Pastel Bouquet",
+    price: 3200,
+    image: "images/mixed.jpg",
+    description: "Beautiful mix of pastel flowers"
+  }
+];
